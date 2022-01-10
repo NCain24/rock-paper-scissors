564d675c753a91740c1123d9e33690b6f7138396
Noah Cain <noahcain.work@gmail.com>
2022-01-10 11:53:33 -0800

    Revised rock paper scissors

--- app.js
+++ app.js
@@ -1,44 +1,74 @@
-const choices = ['rock', 'paper', 'scissors'];
+let pChoice;
+let cChoice = {Value:''};
+let pChoiceInt = 0;
+let cChoiceInt = 0;
+const buttons = document.querySelectorAll('.btn');
 
-function computerPlay() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
+let pScore = 0;
+let cScore = 0;
 
-function playerPlay() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-    
-function playRound() {
-    const playerSelection = playerPlay();
-    const computerSelection = computerPlay();
-        
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
-            return (`You lose! ${computerSelection} beats ${playerSelection}.`);
-        } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-                return (`You win! ${playerSelection} beats ${computerSelection}.`);
-        } else if (playerSelection == 'rock' && computerSelection == 'rock') {
-                return (`Tie! You both chose ${playerSelection}.`);
-        } else if (playerSelection == 'paper' && computerSelection == 'paper') {
-                return (`Tie! You both chose ${playerSelection}.`);
-        } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-                return (`You lose! ${computerSelection} beats ${playerSelection}.`);
-        } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-                return (`You win! ${playerSelection} beats ${computerSelection}.`);
-        } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-                return (`You win! ${playerSelection} beats ${computerSelection}.`);
-        } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-                return (`Tie! You both chose ${playerSelection}.`);
-        } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-                return (`You lose! ${computerSelection} beats ${playerSelection}.`);
+const player = document.querySelector('#player-score');
+player.textContent = `Player Score: ${pScore}`;
+
+const computer = document.querySelector('#comp-score');
+computer.textContent = `Computer Score: ${cScore}`;
+
+const output = document.querySelector('#output');
+
+buttons.forEach((button) => (button.addEventListener)('click',() => {
+        pChoice = button.id;
+        if (pChoice === 'rock') {
+                pChoiceInt = 0;
+        } else if (pChoice === 'paper') {
+                pChoiceInt = 1;
+        } else if (pChoice === 'scissors') {
+                pChoiceInt = 2;
         }
-    }
-    
-    function game() {
-        console.log(playRound());
-        console.log(playRound());
-        console.log(playRound());
-        console.log(playRound());
-        console.log(playRound());
-    }
+        
+        cChoiceInt = cPlay(cChoice);
+        playGame();
+}))
 
-    console.log(game());
+
+
+function cPlay(cChoice) {
+        let cNum = Math.floor(Math.random() * 3);
+        if (cNum === 0) {
+                cChoice.Value = 'rock';
+        } else if (cNum === 1) {
+                cChoice.Value = 'paper';
+        } else if (cNum === 2) {
+                cChoice.Value = 'scissors';
+        }
+        return cNum;
+}
+
+function playRound() {
+        let winner = [[0, 2, 1], [1, 0, 2], [2, 1, 0]];
+        let result = winner[pChoiceInt][cChoiceInt];
+        if (result === 0) {
+                output.textContent = `${pChoice} : ${cChoice.Value}`;
+        } else if (result === 1) {
+                output.textContent = `${pChoice} : ${cChoice.Value}`;
+                pScore++
+        } else if (result === 2) {
+                output.textContent = `${pChoice} : ${cChoice.Value}`;
+                cScore++
+        }
+}
+
+function playGame() {
+        output.textContent = 'Make a choice.';
+        playRound();
+        player.textContent = `Player Score: ${pScore}`;
+        computer.textContent = `Computer Score: ${cScore}`;
+        if (pScore === 5) {
+                output.textContent = 'YOU WIN!!!';
+                pScore = 0;
+                cScore = 0;
+        } else if (cScore === 5) {
+                output.textContent = 'Better luck next time...';
+                pScore = 0;
+                cScore = 0;
+        }
+}
